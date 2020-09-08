c7ff4e879c8be1846259c50c5a8d3579b09da79d
ChetanTayal138 <chetantayal138@gmail.com>
2020-09-08 23:14:39 +0530

    fix grayscale render in html page

--- app.py
+++ app.py
@@ -10,17 +10,23 @@ import tensorflow.keras as keras
 import sys
 import matplotlib.pyplot as plt
 import cv2
+from PIL import Image
 
 app = Flask(__name__)
 
 
 @app.before_first_request
 def before_first_request():
-    """Load models before first request."""
-
+    """Load models before first sending first request."""
+    global encoder
+    global decoder
+    encoder = keras.models.load_model("models/encoder")
+    decoder = keras.models.load_model("models/decoder")
+    print("LOADED MODELS")
 
 @app.route('/', methods=["GET", "POST"])
 def upload_file():
+    """Renders page for uploading the image."""
     return render_template("upload.html")
 
 
@@ -29,44 +35,34 @@ def get_file():
     """Save uploaded file to server."""
     if request.method == 'POST':
         f = request.files['file']
-        f.save(os.path.join("uploads",secure_filename(f.filename)))
+        f.save(os.path.join("static/uploads",secure_filename(f.filename)))
         print("IMAGE SAVED : ",f.filename )
         return redirect(url_for('image_classifier', filename=f.filename))
 
-@app.before_first_request
-def before_first_request():
-    """Load models before first request."""
-    global encoder
-    global decoder
-    encoder = keras.models.load_model("models/encoder")
-    decoder = keras.models.load_model("models/decoder")
-    print("LOADED MODELS")
 
 @app.route('/predict/<filename>', methods=['GET'])
 def image_classifier(filename):
-    """Endpoint for returning prediction json."""
-
-    loaded_img = os.path.join("data",filename)
-    img = cv2.imread(loaded_img, cv2.IMREAD_GRAYSCALE)/255.
-    print("LOADED IMAGE OF SHAPE : ", img.shape)
-    imgs = []
-    imgs.append(img)
-    imgs = np.array(imgs)
-    imgs = imgs.reshape(imgs.shape[0],imgs.shape[1],imgs.shape[2],1)
-    print(imgs.shape)
-
-    encoded_imgs = encoder.predict(imgs)
-    print("ENCODED IMAGES")
-    print(encoded_imgs.shape)
-    decoded_imgs = decoder.predict(encoded_imgs)
-    print(decoded_imgs.shape)
-
-    plt.imsave("predictions/predicted.jpg", decoded_imgs[0].reshape(28,28))
-
+    """Carries out conversion from noisy to clean image."""
+    upload_path = os.path.join("static/uploads", filename)
+    save_path = os.path.join("static/predictions", filename)
     
-    return "Success"
-    #return jsonify(inception_v3.decode_predictions(np.array(pred['predictions']))[0])
+    img = plt.imread(upload_path)/255. # Divide by 255. to normalize pixel values between 0 and 1 as our model has been trained on the same distribution
+    r,g,b = img[:,:,0], img[:,:,1], img[:,:,2]
+    img = 0.2989*r + 0.5870*g + 0.1140*b # Convert the image to grayscale
+    
+    imgs = np.array([img])
+    imgs = imgs.reshape(imgs.shape[0],imgs.shape[1],imgs.shape[2],1)
 
+    # Encode the image through our encoder block
+    encoded_imgs = encoder.predict(imgs)
+    # Decode image through decoder block that has the layers made up from our trained autoencoder
+    decoded_imgs = decoder.predict(encoded_imgs)
+    
+
+    # Save the result in our predictions folder
+    plt.imsave(save_path, decoded_imgs[0].reshape(28,28))
+
+    return render_template('index.html', noisy_image=upload_path,denoised_image=save_path)
 
 
 if __name__ == "__main__":
